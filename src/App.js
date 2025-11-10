7b4f358fd05102dad3ac1435e90429886c519aa3
watermelonstrawbery <sainas@kth.se>
2025-11-10 18:41:39 +0100

    initial layout for the event boxes done

--- src/App.js
+++ src/App.js
@@ -61,24 +61,10 @@ function App() {
   return (
 
     <div className="App">
-      <header className="App-header">
+      <header className="event-box">
         {events.map( 
           (event) => <EventHandler event={event}/>
         )}
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-         
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/EventHandler.js
+++ src/EventHandler.js
@@ -1,12 +1,18 @@
+import './App.css';
+
 export function EventHandler(props){
   return<div>
       <br/>
+      Event name:
       {props.event.title}
       <br/>
+      Date:
       {props.event.date}
       <br/>
+      Style:
       {props.event.style}
       <br/>
+      Location:
       {props.event.location}
       <br/>
     </div>
