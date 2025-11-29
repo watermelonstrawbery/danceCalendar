d8a4c8d3ffb35a52fe4831169e8cba0a82226aea
watermelonstrawbery <sainas@kth.se>
2025-11-29 14:27:36 +0100

    css fixes

--- src/EventHandler.js
+++ src/EventHandler.js
@@ -5,7 +5,6 @@ export function EventHandler(props){
   let formatedDate = new Date(props.event.date).toDateString();
 
   return<div className='event-box'>
-      <br/>
       Event name:
       {props.event.title}
       <br/>
@@ -17,7 +16,6 @@ export function EventHandler(props){
       <br/>
       Location:
       {props.event.location}
-      <br/>
     </div>
 }
 
